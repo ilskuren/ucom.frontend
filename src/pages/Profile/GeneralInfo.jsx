c6e5f6e76f8e824c96325c424a453afdf9cab3f7
Pavel Ivanov <sudokey@gmail.com>
2018-08-14 16:14:04 +0300

    Fixed layout

--- src/pages/Profile/GeneralInfo.jsx
+++ src/pages/Profile/GeneralInfo.jsx
@@ -8,15 +8,17 @@ const ProfileGeneralInfoPage = () => (
     <Header />
 
     <div className="content">
-      <div className="content__vertical-menu">
-        <VerticalMenu sections={[{ type: 'blockchain', percents: '0' }, { type: 'work', percents: '0' },
-        { type: 'education', percents: '0' }]}
-        />
-      </div>
-      <div className="content__person-form">
-        <PersonForm>
-          Content
-        </PersonForm>
+      <div className="content__inner content__inner_flex">
+        <div className="content__vertical-menu">
+          <VerticalMenu sections={[{ type: 'blockchain', percents: '0' }, { type: 'work', percents: '0' },
+          { type: 'education', percents: '0' }]}
+          />
+        </div>
+        <div className="content__person-form">
+          <PersonForm>
+            Content
+          </PersonForm>
+        </div>
       </div>
     </div>
   </div>

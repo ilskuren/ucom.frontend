6064d411329eeb1b32456c177ebd28663f60f880
Alexey Zhmaylik <barbuhs2@gmail.com>
2018-08-14 18:33:25 +0700

    WIP General-info

--- src/pages/Profile/GeneralInfo.jsx
+++ src/pages/Profile/GeneralInfo.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import Header from '../../components/Header';
+import VerticalMenu from '../../components/VerticalMenu';
+import PersonForm from '../../components/PersonForm';
 
 const ProfileGeneralInfoPage = () => (
   <div className="page">
     <Header />
 
     <div className="content">
-      General info
+        <div className="content__vertical-menu">
+          <VerticalMenu sections={[{ type: 'blockchain', percents: '0' }, { type: 'work', percents: '0' },
+          { type: 'education', percents: '0' }]}
+          />
+        </div>
+        <div className="content__person-form">
+          <PersonForm>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+          </PersonForm>
+        </div>
+      </div>
     </div>
-  </div>
 );
 
 export default ProfileGeneralInfoPage;

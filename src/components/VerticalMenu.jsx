e065ffa3ea15e6bb48352beb48845f7d017f048e
Alexey Zhmaylik <barbuhs2@gmail.com>
2018-08-14 15:03:41 +0700

    switch to classNames

--- src/components/VerticalMenu.jsx
+++ src/components/VerticalMenu.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const VerticalMenu = (props) => {
   const sections = props.sections.map((section, i) => (
-    <li className={`vertical-menu__section ${i === 0 ? 'vertical-menu__section_active' : ''}`} key={i}>
+    <li
+      className={classNames('vertical-menu__section', {
+      'vertical-menu__section_active': i === 0,
+      })}
+      key={i}
+    >
       <span className="vertical-menu__type">{section.type}</span>
       <span className="vertical-menu__percentages">{`${section.percentages}%`}</span>
     </li>));

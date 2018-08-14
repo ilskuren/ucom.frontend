4c0ae90e4da8baf820556d4ed231b8b09157a76a
Artem Feoktistov <ognellis2@mail.ru>
2018-08-14 18:04:47 +0700

    create DateInput

---
+++ src/components/DateInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classNames from 'classnames';
+import TextInput from '../components/TextInput';
+import Dropdown from '../components/Dropdown';
+
+const DateInput = () => (
+  <div>
+    <TextInput />
+    <Dropdown />
+    <TextInput />
+  </div>
+);
+
+export default DateInput;

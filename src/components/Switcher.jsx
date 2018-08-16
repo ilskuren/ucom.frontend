41bd2dc4dbae788275ea975792617e1b78964b67
Artem Feoktistov <ognellis2@mail.ru>
2018-08-16 15:10:55 +0700

    add switcher

---
+++ src/components/Switcher.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Switcher = ({ isChecked }) => (
+  <div>11</div>
+);
+
+Switcher.propTypes = {
+  isChecked: PropTypes.bool,
+};
+
+Switcher.defaultProps = {
+  isChecked: false,
+};
+
+export default Switcher;

0caef80dd52339b82e3311cb0471fec94f71c6c4
Artem Feoktistov <ognellis2@mail.ru>
2018-08-16 15:56:19 +0700

    add animation

--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Switcher = ({ isChecked }) => (
-  <div>11</div>
+  <div className="switcher">
+    <label className="switcher__label">
+      <span>Off</span>
+      <input className="switcher__input" type="checkbox" value={isChecked} />
+      <div className="switcher__checkmark">
+        <div className="switcher__toggle" />
+      </div>
+      <span>On</span>
+    </label>
+  </div>
 );
 
 Switcher.propTypes = {

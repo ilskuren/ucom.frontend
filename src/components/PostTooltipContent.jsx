e7bfee53c2d34031e4069778606623cd2f6a72bb
Artem Feoktistov <ognellis2@mail.ru>
2018-08-20 20:15:58 +0700

    post tooltip

--- src/components/PostTooltipContent.jsx
+++ src/components/PostTooltipContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import Link from './Link';
 
 const PostTooltipContent = () => (
@@ -9,15 +10,44 @@ const PostTooltipContent = () => (
     <div className="post-tooltip-content__share">
       <div className="post-tooltip-content__share-title">Share to </div>
       <div className="post-tooltip-content__social">
-        <div className="post-tooltip-content__social-element" />
-        <div className="post-tooltip-content__social-element" />
-        <div className="post-tooltip-content__social-element" />
-        <div className="post-tooltip-content__social-element" />
+        <div className={cn(
+          'post-tooltip-content__social-element',
+          {
+            'post-tooltip-content__social-element_fb': true,
+          },
+          )}
+        />
+        <div className={cn(
+          'post-tooltip-content__social-element',
+          {
+            'post-tooltip-content__social-element_twitter': true,
+          },
+          )}
+        />
+        <div className={cn(
+          'post-tooltip-content__social-element',
+          {
+            'post-tooltip-content__social-element_telegram': true,
+          },
+          )}
+        />
+        <div className={cn(
+          'post-tooltip-content__social-element',
+          {
+            'post-tooltip-content__social-element_vk': true,
+          },
+          )}
+        />
       </div>
     </div>
-    <div className="post-tooltip-content__link">
-      <div className="post-tooltip-content__link-title">Copy link</div>
-      <Link href="http://shpargalkablog.ru/2011/06/css-box-shadow.html" />
+    <div className="post-tooltip-content__share-link">
+      <div className="post-tooltip-content__share-link-title">Copy link</div>
+      <div className="post-tooltip-content__share-link-link">
+        <div className="post-tooltip-content__link">
+          <Link href="http://shpargalkablog.ru/2011/06/css-box-shadow.html" />
+        </div>
+        <div className="post-tooltip-content__copy" />
+      </div>
     </div>
   </div>
 );

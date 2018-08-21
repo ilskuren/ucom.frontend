134d080459423ad2cd2191d42069186e405c5dfd
shevdi <shevdi22@rambler.ru>
2018-08-21 16:46:27 +0700

    converted posts page in functional component

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,31 +1,23 @@
 import { Route } from 'react-router';
-import React, { PureComponent, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import StoryPage from './Posts/Story';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-class PostsPage extends PureComponent {
-  componentDidMount() {
+const PostsPage = () => (
+  <div className="page">
+    <Header />
 
-  }
+    <div className="content">
+      <div className="content__inner">
+        <Fragment>
+          <Route exact path="/posts/story" component={StoryPage} />
+        </Fragment>
 
-  render() {
-    return (
-      <div className="page">
-        <Header />
-
-        <div className="content">
-          <div className="content__inner">
-            <Fragment>
-              <Route exact path="/posts/story" component={StoryPage} />
-            </Fragment>
-
-            <Footer />
-          </div>
-        </div>
+        <Footer />
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default PostsPage;

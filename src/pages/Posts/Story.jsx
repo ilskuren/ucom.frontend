3d9c32b3aed72b5fdb31d411c1d680f7ab2f165d
Artem Feoktistov <ognellis2@mail.ru>
2018-08-22 17:39:52 +0700

    complete text editor in story

--- src/pages/Posts/Story.jsx
+++ src/pages/Posts/Story.jsx
@@ -3,7 +3,9 @@ import TextEditor from '../../components/TextEditor';
 
 const StoryPage = () => (
   <div className="create-story">
-    <TextEditor />
+    <div className="create-story__text-editor">
+      <TextEditor />
+    </div>
   </div>
 );
 

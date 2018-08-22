f423c959a5244546c21e69b09139f9ad4c22ba54
Alexey Zhmaylik <barbuhs2@gmail.com>
2018-08-22 14:55:43 +0700

    Minor fix

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -3,14 +3,15 @@ import propTypes from 'prop-types';
 
 const RadioButton = props => (
   <div className="radio-button">
-    <input id={props.id} className="radio-button__radio" name="radio" type="radio" />
-    <label htmlFor={props.id} className="radio-button__label">{props.label}</label>
+    <input id={props.id} className="radio-button__radio" name={props.name} type="radio" />
+    { props.label && <label htmlFor={props.id} className="radio-button__label">{props.label}</label> }
   </div>
 );
 
 RadioButton.propTypes = {
   id: propTypes.string,
   label: propTypes.string,
+  name: propTypes.string,
 };
 
 export default RadioButton;

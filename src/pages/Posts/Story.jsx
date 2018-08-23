176ee4df51cd258c8c374e2aaac683e92d1d108a
shevdi <shevdi22@rambler.ru>
2018-08-23 15:37:35 +0700

    added title in story page

--- src/pages/Posts/Story.jsx
+++ src/pages/Posts/Story.jsx
@@ -1,10 +1,17 @@
 
 import React from 'react';
+import PostItem from '../../components/PostItem';
 
 const StoryPage = () => (
   <div className="posts">
     <div className="posts__content">
-      Content
+      <PostItem
+        title="No Country for Old Men, aren't it"
+        tag="story"
+        rate={9200}
+        size="big"
+        edit
+      />
     </div>
     <div className="posts__sidebar">
       Sidebar

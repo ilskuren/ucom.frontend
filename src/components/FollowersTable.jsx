62e26978aa8b41ad0a209199666bf3bfc4283cc5
Alexey Zhmaylik <barbuhs2@gmail.com>
2018-08-27 20:00:05 +0700

    Add followers table

--- src/components/FollowersTable.jsx
+++ src/components/FollowersTable.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import UserCard from '../components/UserCard';
 import Button from '../components/Button';
 
+// In production you can remove Math.floor(Math.random() * 10000) + 1
 const renderFollowersTableRow = ({
   userCardData: { userName, accountName, avatarUrl }, rate, followers, trustedBy,
 }) => (
-  <div className="followers-table__row">
+  <div className="followers-table__row" key={accountName + Math.floor(Math.random() * 10000) + 1}>
     <div className="followers-table__user">
       <UserCard
         userName={userName}
@@ -15,15 +16,20 @@ const renderFollowersTableRow = ({
       />
     </div>
     <div className="followers-table__numbers">
-      {followers}
-    </div>
-    <div className="followers-table__numbers">
-      {trustedBy}
-    </div>
-    <div className="rate">
-      <div className="rate__value">
-        {rate}
-        <span class="rate__degree">°</span>
+      <div className="followers-table__number">
+        <div className="followers-table__column-name followers-table__column-name_in-row">followers</div>{followers}
+      </div>
+      <div className="followers-table__number">
+        <div className="followers-table__column-name followers-table__column-name_in-row">trusted by</div>{trustedBy}
+      </div>
+      <div className="followers-table__number">
+      <div className="followers-table__column-name followers-table__column-name_in-row">rate</div>
+        <div className="rate">
+          <div className="rate__value followers-table__mobile-rate-value">
+            {rate}
+            <span className="rate__degree">°</span>
+          </div>
+        </div>
       </div>
     </div>
     <div className="followers-table__button">
@@ -32,9 +38,18 @@ const renderFollowersTableRow = ({
   </div>
 );
 
+const renderFollowersTableHeader = () => (
+  <div className="followers-table__header">
+    <div className="followers-table__column-name">name</div>
+    <div className="followers-table__column-name">followers<div className="followers-table__triangle"/></div>
+    <div className="followers-table__column-name">trusted by<div className="followers-table__triangle"/></div>
+    <div className="followers-table__column-name">rate<div className="followers-table__triangle"/></div>
+  </div>
+)
+
 const FollowersTable = ({ users }) => (
   <div className="followers-table">
-
+    {renderFollowersTableHeader()}
     {users.map(renderFollowersTableRow)}
     <div className="followers-table__row followers-table__row_load-more">
       load more
@@ -42,16 +57,7 @@ const FollowersTable = ({ users }) => (
   </div>
 );
 
-const renderFollowersTableHeader = () => (
-  <div className="followers-table__header">
-    <div className="followers-table__name">name</div>
-    <div className="followers-table__followers">name</div>
-    <div className="followers-table__trusted-by">name</div>
-    <div className="followers-table__rate">name</div>
-  </div>
-)
-
-renderFollowerTableRow.propTypes = {
+renderFollowersTableRow.propTypes = {
   userCardData: PropTypes.shape({
     userName: PropTypes.string,
     accountName: PropTypes.string,
--- src/pages/MyProfile/Followers.jsx
+++ src/pages/MyProfile/Followers.jsx
@@ -13,19 +13,6 @@ const users = Array.from({ length: 5 }, () => (
     rate: 10800,
   }));
 
-// const users = [
-//   {
-//     userCardData: {
-//       userName: 'Jason Born',
-//       accountName: 'apple_inc',
-//       avatarUrl: 'https://cdn-images-1.medium.com/fit/c/300/300/1*28Gx-SixWGfev_WLLuCfhg.jpeg',
-//     },
-//     followers: 8923,
-//     trustedBy: 8923,
-//     rate: 10800,
-//   }
-// ]
-
 const FollowersPage = () => (
   <div className="followers">
     <FollowersTable users={users} />

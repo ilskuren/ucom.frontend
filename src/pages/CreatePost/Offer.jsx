de49f1a2d370e4ade5dadf6429ba4c364c448e21
shevdi <shevdi22@rambler.ru>
2018-08-28 17:48:45 +0700

    added block with menu

--- src/pages/CreatePost/Offer.jsx
+++ src/pages/CreatePost/Offer.jsx
@@ -49,7 +49,7 @@ class StoryPage extends PureComponent {
           </div>
           <div className="form">
             <div className="form__block">
-              <div className="form__label">Name Media Post</div>
+              <div className="form__label form__label_color_gray">Name Media Post</div>
               <div className="form__input">
                 <PrefixInput
                   prefix="u.community/"
@@ -57,45 +57,84 @@ class StoryPage extends PureComponent {
                 />
               </div>
             </div>
-            <div className="form__block">
-              <div className="form__label">Offer Title</div>
+            <div className="form__block form__block_shifted_down">
+              <div className="menu menu_simple-tabs menu_simple-tabs_black menu_simple-tabs_small">
+                <div className="menu__item">
+                  <div className="menu__link menu__link_black menu__link_active">
+                    Sale
+                  </div>
+                </div>
+                <div className="menu__item">
+                  <div className="menu__link menu__link_black">
+                    Token Sale
+                  </div>
+                </div>
+                <div className="menu__item">
+                  <div className="menu__link menu__link_black">
+                    Event
+                  </div>
+                </div>
+                <div className="menu__item">
+                  <div className="menu__link menu__link_black">
+                    Service
+                  </div>
+                </div>
+                <div className="menu__item">
+                  <div className="menu__link menu__link_black">
+                    White List
+                  </div>
+                </div>
+                <div className="menu__item">
+                  <div className="menu__link menu__link_black">
+                    Charity
+                  </div>
+                </div>
+                <div className="menu__item">
+                  <div className="menu__link menu__link_black">
+                    Give Away
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="form__block form__block_shifted_down">
+              <div className="form__label form__label_color_gray">Offer Title</div>
               <div className="form__input">
                 <TextInput placeholder="Type something..." />
               </div>
             </div>
             <div className="form__block form__block_shifted_down">
-              <div className="form__label">Action Button</div>
+              <div className="form__label form__label_color_gray">Action Button</div>
               <div className="form__input">
                 <TextInput placeholder="Name of Acton Button" />
               </div>
             </div>
             <div className="form__block  form__block_shifted_up">
-              <div className="form__label" />
+              <div className="form__label form__label_color_gray" />
               <div className="form__input">
                 <TextInput placeholder="Link" />
               </div>
             </div>
             <div className="form__block form__block_shifted_down">
-              <div className="form__label">Time Sale</div>
+              <div className="form__label form__label_color_gray">Time Sale</div>
               <div className="form__input">
                 <TextInput placeholder="Days" inputWidth={150} />
               </div>
             </div>
             <div className="form__block form__block_no-label">
-              <div className="form__label" />
+              <div className="form__label form__label_color_gray" />
               <div className="form__input">
-                <span className="form__input-label">Unlimited</span>
+                <span className="form__input-label form__input-label_color_gray">Unlimited</span>
                 <Switcher withoutLabels />
               </div>
             </div>
             <div className="form__block form__block_shifted_down">
-              <div className="form__label">Add Team</div>
+              <div className="form__label form__label_color_gray">Add Team</div>
               <div className="form__input">
                 <TextInput placeholder="Find People" />
               </div>
             </div>
             <div className="form__block form__block_shifted_down">
-              <div className="form__label">Offer&apos;s cover</div>
+              <div className="form__label form__label_color_gray">Offer&apos;s cover</div>
               <div className="form__input">
                 <DropZone text="add or drag img" />
                 <div className="form__subtext">

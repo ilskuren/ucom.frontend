184131e01587f6a36ad797440eaa5d50cc378620
Alexey Zhmaylik <barbuhs2@gmail.com>
2018-08-28 12:21:46 +0700

    Add check icon for button

--- src/components/FollowersTable.jsx
+++ src/components/FollowersTable.jsx
@@ -3,49 +3,59 @@ import PropTypes from 'prop-types';
 import UserCard from '../components/UserCard';
 import Button from '../components/Button';
 
-// In production you can remove Math.floor(Math.random() * 10000) + 1
+// In production you should remove randomNumber
 const renderFollowersTableRow = ({
   userCardData: { userName, accountName, avatarUrl }, rate, followers, trustedBy,
-}) => (
-  <div className="followers-table__row" key={accountName + Math.floor(Math.random() * 10000) + 1}>
-    <div className="followers-table__user">
-      <UserCard
-        userName={userName}
-        accountName={accountName}
-        avatarUrl={avatarUrl}
-      />
-    </div>
-    <div className="followers-table__numbers">
-      <div className="followers-table__number">
-        <div className="followers-table__column-name followers-table__column-name_in-row">followers</div>{followers}
+}) => {
+  const randomNumber = Math.floor(Math.random() * 10000) + 1;
+  const conditionForDemonstration = randomNumber % 2 === 0;
+  return (
+    <div className="followers-table__row" key={accountName + randomNumber}>
+      <div className="followers-table__user">
+        <UserCard
+          userName={userName}
+          accountName={accountName}
+          avatarUrl={avatarUrl}
+        />
       </div>
-      <div className="followers-table__number">
-        <div className="followers-table__column-name followers-table__column-name_in-row">trusted by</div>{trustedBy}
-      </div>
-      <div className="followers-table__number">
-      <div className="followers-table__column-name followers-table__column-name_in-row">rate</div>
-        <div className="rate">
-          <div className="rate__value followers-table__mobile-rate-value">
-            {rate}
-            <span className="rate__degree">°</span>
+      <div className="followers-table__numbers">
+        <div className="followers-table__number">
+          <div className="followers-table__column-name followers-table__column-name_in-row">followers</div>{followers}
+        </div>
+        <div className="followers-table__number">
+          <div className="followers-table__column-name followers-table__column-name_in-row">trusted by</div>{trustedBy}
+        </div>
+        <div className="followers-table__number">
+          <div className="followers-table__column-name followers-table__column-name_in-row">rate</div>
+          <div className="rate">
+            <div className="rate__value followers-table__mobile-rate-value">
+              {rate}
+              <span className="rate__degree">°</span>
+            </div>
           </div>
         </div>
       </div>
+      <div className="followers-table__button">
+        <Button
+          text={conditionForDemonstration ? 'Following' : 'Follow' }
+          size="medium"
+          theme="transparent"
+          isStretched
+          withCheckedIcon={conditionForDemonstration}
+        />
+      </div>
     </div>
-    <div className="followers-table__button">
-      <Button text="Follow" size="medium" theme="transparent" isStretched />
-    </div>
-  </div>
-);
+  );
+};
 
 const renderFollowersTableHeader = () => (
   <div className="followers-table__header">
     <div className="followers-table__column-name">name</div>
-    <div className="followers-table__column-name">followers<div className="followers-table__triangle"/></div>
-    <div className="followers-table__column-name">trusted by<div className="followers-table__triangle"/></div>
-    <div className="followers-table__column-name">rate<div className="followers-table__triangle"/></div>
+    <div className="followers-table__column-name">followers<div className="followers-table__triangle" /></div>
+    <div className="followers-table__column-name">trusted by<div className="followers-table__triangle" /></div>
+    <div className="followers-table__column-name">rate<div className="followers-table__triangle" /></div>
   </div>
-)
+);
 
 const FollowersTable = ({ users }) => (
   <div className="followers-table">

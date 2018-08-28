8a07c95008bbbf9040abab77c922a31ddc9b3816
Alexey Zhmaylik <barbuhs2@gmail.com>
2018-08-28 14:44:16 +0700

    Add text editor to offer page

--- src/pages/CreatePost/Offer.jsx
+++ src/pages/CreatePost/Offer.jsx
@@ -4,24 +4,45 @@ import React, { Fragment, PureComponent } from 'react';
 import CreatePostHeader from '../../components/CreatePostHeader';
 import Loading from '../../components/Loading';
 import CreatePostFooter from '../../components/CreatePostFooter';
+import TextEditor from '../../components/TextEditor';
+import { getToken } from '../../utils/token';
+import { createPost } from '../../api';
 
 class StoryPage extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
+      title: '',
+      description: '',
+      leading_text: '',
       newPostId: null,
+      main_image_filename: null,
       loading: false,
       saved: false,
     };
   }
 
   save() {
-    this.setState({
-      loading: false,
-      saved: true,
-      newPostId: 1,
-    });
+    this.setState({ loading: true });
+
+    const token = getToken();
+    const data = new FormData();
+
+    data.append('title', this.state.title);
+    data.append('description', this.state.description);
+    data.append('leading_text', this.state.leading_text);
+    data.append('main_image_filename', this.state.main_image_filename);
+    data.append('post_type_id', 1);
+
+    createPost(data, token)
+      .then((post) => {
+        this.setState({
+          loading: false,
+          saved: true,
+          newPostId: post.id,
+        });
+      });
   }
 
   render() {
@@ -36,6 +57,18 @@ class StoryPage extends PureComponent {
           onClickPost={() => { this.save(); }}
           withoutTabs
         />
+
+        <div className="create-post__editor">
+          <TextEditor
+            title={this.state.title}
+            description={this.state.description}
+            leadingText={this.state.leading_text}
+            onChangeTitle={title => this.setState({ title })}
+            onChangeDescription={description => this.setState({ description })}
+            onChangeLeadingText={leading_text => this.setState({ leading_text })}
+            onChangeCover={main_image_filename => this.setState({ main_image_filename })}
+          />
+        </div>
 
         <CreatePostFooter onClickPost={() => this.save()} />
       </Fragment>

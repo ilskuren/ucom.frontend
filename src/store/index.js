b1657a900572104f57b93aa3691f7ee28a9f0434
krashaen <krashaen@gmail.com>
2018-09-04 12:09:04 +0700

    add redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import user from './user';
 import offer from './offer';
 import settings from './settings';
 
 const app = combineReducers({ user, offer, settings });
+const middlewares = [];
 
-export default createStore(app);
+const store = createStore(app, composeWithDevTools(applyMiddleware(...middlewares)));
+
+export default store;

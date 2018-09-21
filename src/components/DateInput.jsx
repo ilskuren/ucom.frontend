00eafac7cc4aeee534559f5cd0e29c6dfd3a43be
shevdi <shevdi22@rambler.ru>
2018-09-21 14:51:57 +0700

    made true amount of days in date input

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -2,6 +2,7 @@ import { range } from 'lodash';
 import Select from 'react-select';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import dict from '../utils/dict';
 
 class DateInput extends PureComponent {
@@ -14,6 +15,7 @@ class DateInput extends PureComponent {
       day: date ? date[2] : '',
       month: date ? date[1] : '',
       year: date ? date[0] : '',
+      daysInMonth: this.getDaysInMonth(date ? date[0] : 2000, date ? date[1] : '01', date ? date[2] : 31),
     };
   }
 
@@ -22,13 +24,26 @@ class DateInput extends PureComponent {
       return;
     }
 
+    this.setState({ daysInMonth: this.getDaysInMonth(this.state.year, this.state.month, this.state.day) });
     if (this.state.day && this.state.month && this.state.year) {
       this.props.onChange(`${this.state.year}-${this.state.month}-${this.state.day}`);
     }
   }
 
+  getDaysInMonth(year, month, day) {
+    const daysInMonth = moment(`${year || '2000'}-${month || '01'}-01`).daysInMonth();
+    if (daysInMonth < day) {
+      this.setState({ day: String(daysInMonth) });
+    }
+    return daysInMonth;
+  }
+
+  convertToTimeString(time) {
+    return `${time < 10 ? '0' : ''}${time}`;
+  }
+
   render() {
-    const days = range(1, 32).map(i => ({ value: i, label: i }));
+    const days = range(1, this.state.daysInMonth + 1).map(i => ({ value: i, label: i }));
     const years = range(2018, 1905).map(i => ({ value: i, label: i }));
 
     return (
@@ -43,7 +58,7 @@ class DateInput extends PureComponent {
             options={days}
             value={days.find(i => +i.value === +this.state.day)}
             onChange={(item) => {
-              this.setState({ day: item.value }, () => {
+              this.setState({ day: this.convertToTimeString(item.value) }, () => {
                 this.onChange();
               });
             }}
@@ -58,7 +73,7 @@ class DateInput extends PureComponent {
             options={dict.months}
             value={dict.months.find(i => +i.value === +this.state.month)}
             onChange={(item) => {
-              this.setState({ month: item.value }, () => {
+              this.setState({ month: this.convertToTimeString(item.value) }, () => {
                 this.onChange();
               });
             }}
@@ -73,7 +88,7 @@ class DateInput extends PureComponent {
             options={years}
             value={years.find(i => +i.value === +this.state.year)}
             onChange={(item) => {
-              this.setState({ year: item.value }, () => {
+              this.setState({ year: String(item.value) }, () => {
                 this.onChange();
               });
             }}

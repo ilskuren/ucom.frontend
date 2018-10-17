a26d8c3a9268654de2d252cd582ae425072090a7
Pavel Ivanov <sudokey@gmail.com>
2018-10-17 11:42:15 +0300

    Fixes

--- src/components/NotificationTooltip.jsx
+++ src/components/NotificationTooltip.jsx
@@ -15,47 +15,55 @@ const filterNotifs = (arr, isEarly = true) => Object.values(arr)
   .filter(i => (i.finished || i.seen) === isEarly)
   .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
 
-const NotificationTooltip = ({ list, hideTooltip }) => (
-  <PerfectScrollbar className="notification-tooltip__container">
-    {isRequiredTime(list, false) &&
-      <div className="notification-tooltip__header notification-tooltip__header_new">
-        <h3 className="notification-tooltip__title">New notifications</h3>
-      </div>
+const NotificationTooltip = ({ list, hideTooltip }) => {
+  const newNotifications = filterNotifs(list, false);
+  const oldNotifications = filterNotifs(list, true);
+
+  return (
+    <PerfectScrollbar className="notification-tooltip__container">
+      {isRequiredTime(list, false) &&
+        <div className="notification-tooltip__header notification-tooltip__header_new">
+          <h3 className="notification-tooltip__title">New notifications</h3>
+        </div>
       }
 
-    <div className="notification-tooltip__list notification-tooltip__list_new">
-      {filterNotifs(list, false).map(item => (
-        <div key={item.id} className="notification-tooltip__item notification-tooltip__item_new">
-          <NotificationCard
-            {...item}
-          />
+      {newNotifications && newNotifications.length > 0 && (
+        <div className="notification-tooltip__list notification-tooltip__list_new">
+          {newNotifications.map(item => (
+            <div key={item.id} className="notification-tooltip__item notification-tooltip__item_new">
+              <NotificationCard {...item} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-    {isRequiredTime(list, true) &&
-      <div className="notification-tooltip__header">
-        <h3 className="notification-tooltip__title">Early</h3>
-      </div>
-      }
-    <div className="notification-tooltip__list">
-      {filterNotifs(list, true).map(item => (
-        <div key={item.id} className="notification-tooltip__item">
-          <NotificationCard
-            {...item}
-          />
-        </div>
-        ))}
-    </div>
+      )}
 
-    <div
-      className="inline__item notification-tooltip__close"
-      onClick={() => hideTooltip()}
-      role="presentation"
-    >
-      <IconClose />
-    </div>
-  </PerfectScrollbar>
-);
+      {isRequiredTime(list, true) &&
+        <div className="notification-tooltip__header">
+          <h3 className="notification-tooltip__title">Early</h3>
+        </div>
+      }
+
+      {oldNotifications && oldNotifications.length > 0 && (
+        <div className="notification-tooltip__list">
+          {oldNotifications.map(item => (
+            <div key={item.id} className="notification-tooltip__item">
+              <NotificationCard {...item} />
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div
+        className="inline__item notification-tooltip__close"
+        onClick={() => hideTooltip()}
+        role="presentation"
+      >
+        <IconClose />
+      </div>
+    </PerfectScrollbar>
+  );
+};
+
 NotificationTooltip.propTypes = {
   hideTooltip: PropTypes.func,
   // addSiteNotifications: PropTypes.func,

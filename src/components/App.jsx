32d8a3591c7bf9c2b2d776a020d1817bb27c4b83
Pavel Ivanov <sudokey@gmail.com>
2018-10-31 16:40:12 +0300

    Added isTrackingAllowed for reg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,10 +18,7 @@ import NotFoundPage from '../pages/NotFoundPage';
 import OrganizationsCreatePage from '../pages/OrganizationsCreate';
 import { setUser, hideAuthPopup } from '../actions';
 import { initNotificationsListeners, setUnreadNotificationsAmount } from '../actions/siteNotifications';
-import { getToken, removeToken } from '../utils/token';
 import { fetchMyself } from '../actions/users';
-import api from '../api';
-import Loading from './Loading';
 import Header from './Header';
 import SignUp from '../pages/SignUp';
 import Page from './Page';
@@ -31,93 +28,58 @@ import Auth from './Auth';
 import Notifications from './Notifications';
 import socket from '../api/socket';
 import config from '../../package.json';
-import gtm from '../utils/gtm.html';
 
 class App extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: true,
-    };
-  }
-
   componentDidMount() {
-    this.restoreSession();
+    this.props.fetchMyself();
     this.props.initNotificationsListeners();
 
     if (config.socketEnabled) {
       socket.connect();
     }
-    if (true && process.env.NODE_ENV === 'production') /* TODO this.props.enabledSendStatistics */ {
-      document.write(gtm);
-    }
-  }
-
-  restoreSession() {
-    const token = getToken();
-
-    this.setState({ loading: true });
-
-    this.props.fetchMyself();
-
-    api.getMyself(token)
-      .then((data) => {
-        this.props.setUser(data);
-        this.props.setUnreadNotificationsAmount(data.unreadMessagesCount);
-      })
-      .catch(() => {
-        removeToken();
-      })
-      .then(() => {
-        this.setState({ loading: false });
-      });
   }
 
   render() {
     return (
       <Fragment>
-        <Loading loading={this.state.loading} appear />
-
         <div id="portal-root" />
-        {!this.state.loading && (
-          <Router history={this.props.history}>
-            <Page>
-              <Header />
 
-              <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/signup" component={SignUp} />
-                <Route path="/profile" component={ProfilePage} />
-                <Route path="/my-profile" component={MyProfilePage} />
-                <Route path="/settings" component={SettingsPage} />
-                <Route path="/notifications" component={NotificationsPage} />
-                <Route exact path="/user/:id" component={UserPage} />
-                <Route exact path="/user/:id/:postId" component={UserPage} />
-                <Route path="/posts/new/:postTypeId" component={CreatePost} />
-                <Route path="/posts/:id/edit" component={CreatePost} />
-                <Route exact path="/posts/:id" component={Post} />
-                <Route path="/publications" component={EventsPage} />
-                <Route path="/users" component={UsersPage} />
-                <Route exact path="/products" component={ProductsPage} />
-                <Route exact path="/communities" component={OrganizationsPage} />
-                <Route exact path="/communities/new" component={OrganizationsCreatePage} />
-                <Route exact path="/communities/:id" component={OrganizationPage} />
-                <Route exact path="/communities/:id/edit" component={OrganizationsCreatePage} />
-                <Route exact path="/communities/:id/:postId" component={OrganizationPage} />
-                <Route component={NotFoundPage} />
-              </Switch>
+        <Router history={this.props.history}>
+          <Page>
+            <Header />
 
-              {this.props.auth.showPopup && (
-                <Popup onClickClose={() => this.props.hideAuthPopup()}>
-                  <Auth onClickClose={() => this.props.hideAuthPopup()} />
-                </Popup>
-              )}
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/profile" component={ProfilePage} />
+              <Route path="/my-profile" component={MyProfilePage} />
+              <Route path="/settings" component={SettingsPage} />
+              <Route path="/notifications" component={NotificationsPage} />
+              <Route exact path="/user/:id" component={UserPage} />
+              <Route exact path="/user/:id/:postId" component={UserPage} />
+              <Route path="/posts/new/:postTypeId" component={CreatePost} />
+              <Route path="/posts/:id/edit" component={CreatePost} />
+              <Route exact path="/posts/:id" component={Post} />
+              <Route path="/publications" component={EventsPage} />
+              <Route path="/users" component={UsersPage} />
+              <Route exact path="/products" component={ProductsPage} />
+              <Route exact path="/communities" component={OrganizationsPage} />
+              <Route exact path="/communities/new" component={OrganizationsCreatePage} />
+              <Route exact path="/communities/:id" component={OrganizationPage} />
+              <Route exact path="/communities/:id/edit" component={OrganizationsCreatePage} />
+              <Route exact path="/communities/:id/:postId" component={OrganizationPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
 
-              <Notifications />
-            </Page>
-          </Router>
-        )}
+            {this.props.auth.showPopup && (
+              <Popup onClickClose={() => this.props.hideAuthPopup()}>
+                <Auth onClickClose={() => this.props.hideAuthPopup()} />
+              </Popup>
+            )}
+
+            <Notifications />
+          </Page>
+        </Router>
       </Fragment>
     );
   }
@@ -125,11 +87,10 @@ class App extends PureComponent {
 
 App.propTypes = {
   history: PropTypes.objectOf(PropTypes.any),
-  setUser: PropTypes.func,
   auth: PropTypes.objectOf(PropTypes.any),
   hideAuthPopup: PropTypes.func,
   initNotificationsListeners: PropTypes.func,
-  setUnreadNotificationsAmount: PropTypes.func,
+  fetchMyself: PropTypes.func,
 };
 
 export default connect(

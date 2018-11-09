7323d0e9614362843ed287f34cb11849aa361c0b
Pavel Ivanov <sudokey@gmail.com>
2018-11-09 11:16:43 +0300

    Fixes

--- src/components/Feed/PostFeedHeader.jsx
+++ src/components/Feed/PostFeedHeader.jsx
@@ -1,17 +1,16 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import PostRating from '../Rating/PostRating';
-// import RepostRating from '../Rating/RepostRating';
 import IconRepost from '../Icons/Repost';
 import UserCard from '../UserCard';
-import { getPostTypeById } from '../../utils/posts';
+import { getPostTypeById, POST_TYPE_REPOST_ID } from '../../utils/posts';
 
 class PostFeedHeader extends PureComponent {
   render() {
     return (
       <div className="post__header">
         <div className="post__info-block">
-          {this.props.postTypeId === 11 ? <IconRepost className="icon-repost" /> : null}
+          {this.props.postTypeId === POST_TYPE_REPOST_ID ? <IconRepost className="icon-repost" /> : null}
           <div className="post__type"><strong>{getPostTypeById(this.props.postTypeId)}</strong></div>
           <div className="toolbar__main">{this.props.updatedAt}</div>
           <div className="toolbar__side">

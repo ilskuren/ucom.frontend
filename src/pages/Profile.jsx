519c8bb2379ef3d250d5911dcaf60825bf3c3c31
andreyelek <sukanov2015@yandex.ru>
2018-11-13 17:18:40 +0300

    markup completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,17 @@
-// import { Element } from 'react-scroll';
 import { connect } from 'react-redux';
-import { Route, Redirect } from 'react-router';
+import { Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
+import { Element } from 'react-scroll';
 import React, { PureComponent, Fragment } from 'react';
 import { selectUser } from '../store/selectors/user';
-// import VerticalMenu from './VerticalMenu';
-import ProfileGeneralInfoPage from './Profile/GeneralInfo';
-// import ProfileWorkAndEducationPage from './Profile/WorkAndEducation';
-import ProfileContactsPage from './Profile/Contacts';
+import VerticalMenu from '../components/VerticalMenu';
+import TextInput from '../components/TextInput';
+import Textarea from '../components/Textarea';
 import Footer from '../components/Footer';
+
+// import ProfileGeneralInfoPage from './Profile/GeneralInfo';
+// import ProfileWorkAndEducationPage from './Profile/WorkAndEducation';
+// import ProfileContactsPage from './Profile/Contacts';
 
 class ProfilePage extends PureComponent {
   componentDidMount() {
@@ -17,40 +20,101 @@ class ProfilePage extends PureComponent {
 
   render() {
     return this.props.user.id ? (
-      <Fragment>
-        <div className="content__inner">
-          <div className="nav-bar nav-bar_simple">
-            <div className="nav-bar__title">
-              <h1 className="title">Edit Profile</h1>
-            </div>
-            <div className="nav-bar__menu">
-              <div className="toolbar toolbar_responsive">
-                <div className="toolbar__side">
-                  <div className="inline">
-                    <div className="inline__item">
-                      <Link to={`/user/${this.props.user.id}`} className="button button_theme_transparent button_size_small">
-                        Back to Profile
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div className="content">
+        <div className="content__inner content__inner_medium">
+          <div className="content__title content__title_between">
+            <h1 className="title">Edit Profile</h1>
+            <Link to={`/user/${this.props.user.id}`} className="button button_theme_transparent button_size_small">
+              Back to Profile
+            </Link>
           </div>
         </div>
 
         <div className="content">
           <div className="content__inner">
             <Fragment>
-              <Route exact path="/profile/general-info" component={ProfileGeneralInfoPage} />
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  // props.setOrganizationActiveTab(STEPS_ID_COMMUNITY);
+                }}
+              >
+                <div className="grid grid_settings">
+                  <div className="grid__item grid__item_side">
+                    <VerticalMenu
+                      sections={[
+                        { name: 'PersonalInfo', title: 'Personal info' },
+                        { name: 'PersonalContacts', title: 'Personal contacts' },
+                        { name: 'SocialNetworks', title: 'Social networks' },
+                      ]}
+                    />
+                  </div>
+                  <div className="grid__item grid__item_main">
+                    <div className="fields">
+                      <Element name="PersonalInfo">
+                        <div className="fields__title">
+                          <h1 className="title title_small">Personal info</h1>
+                        </div>
+
+                        <div className="fields__item">
+                          <div className="field">
+                            <div className="field__label">Displayed name</div>
+                            <div className="field__input">
+                              <TextInput />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="fields__item">
+                          <div className="field">
+                            <div className="field__label">About me</div>
+                            <div className="field__input">
+                              <Textarea
+                                placeholder="Type something..."
+                                rows={6}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </Element>
+                      <Element name="PersonalContacts">
+                        <div className="fields__title">
+                          <h1 className="title title_small">Personal contacts</h1>
+                        </div>
+                        <div className="fields__item">
+                          <div className="field">
+                            <div className="field__label">Your website</div>
+                            <div className="field__input">
+                              <TextInput />
+                            </div>
+                          </div>
+                        </div>
+                      </Element>
+                      <Element name="SocialNetworks">
+                        <div className="fields__title">
+                          <h1 className="title title_small">Social networks</h1>
+                        </div>
+                        <div className="fields__item">
+                          <div className="field">
+                            <div className="field__label">Your website</div>
+                            <div className="field__input">
+                              <TextInput />
+                            </div>
+                          </div>
+                        </div>
+                      </Element>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              {/* <Route exact path="/profile/general-info" component={ProfileGeneralInfoPage} /> */}
               {/* <Route exact path="/profile/work-and-education" component={ProfileWorkAndEducationPage} /> */}
-              <Route exact path="/profile/contacts" component={ProfileContactsPage} />
+              {/* <Route exact path="/profile/contacts" component={ProfileContactsPage} /> */}
             </Fragment>
 
             <Footer />
           </div>
         </div>
-      </Fragment>
+      </div>
     ) : (
       <Redirect to="/" />
     );

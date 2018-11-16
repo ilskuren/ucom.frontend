ab6eb5597602c8676991c6d546a0bf221ba4c24e
Pavel Ivanov <sudokey@gmail.com>
2018-11-16 16:29:43 +0300

    Added governance

--- src/components/Governance/Governance.jsx
+++ src/components/Governance/Governance.jsx
@@ -10,9 +10,9 @@ class Governance extends PureComponent {
     super(props);
 
     this.state = {
-      selectedPanelActive: true,
-      votePanelActive: true,
-      votePopupVisibile: true,
+      selectedPanelActive: false,
+      votePanelActive: false,
+      votePopupVisibile: false,
     };
   }
 
@@ -36,31 +36,33 @@ class Governance extends PureComponent {
                   </div>
 
                   <div className="content__section">
-                    <Panel
-                      title="By completing this transaction, I agree to the following..."
-                      active={this.state.votePanelActive}
-                      onClickToggler={() => this.setState({ votePanelActive: !this.state.votePanelActive })}
-                    >
-                      <div className="governance-vote__text">
-                        <div className="text">
-                          <p>The intent of the ‘voteproducer’ action is to cast a valid vote for up to 30 BP candidates.</p>
-                          <p>As an authorized party I %username% wish to vote on behalf of %username% in favor of the block produser candidates ‘abc1234www’, ‘abc1234www’ with a voting weight equal to all tokens currently owned by %username% and staked for CPU or bandwidth.</p>
-                          <p>If I am not the benefitial owner of these shares I stipulate I have proof that I’ve been authorized to vote these shares by their benefitial owner(s).</p>
-                          <p>I stipulate I have not and will not accept anything of value in exchange for these votes, on penalty of confiscation of these tokens, and other penalties.</p>
-                          <p>I acknowledge that using any system of authomatic voting, re-voting, or vote refreshing, or allowing such system to be used on my behalf or on behalf of another, is forbidden and doing so violates this contract.</p>
+                    <div className="governance-vote__panel">
+                      <Panel
+                        title="By completing this transaction, I agree to the following..."
+                        active={this.state.votePanelActive}
+                        onClickToggler={() => this.setState({ votePanelActive: !this.state.votePanelActive })}
+                      >
+                        <div className="governance-vote__text">
+                          <div className="text">
+                            <p>The intent of the ‘voteproducer’ action is to cast a valid vote for up to 30 BP candidates.</p>
+                            <p>As an authorized party I %username% wish to vote on behalf of %username% in favor of the block produser candidates ‘abc1234www’, ‘abc1234www’ with a voting weight equal to all tokens currently owned by %username% and staked for CPU or bandwidth.</p>
+                            <p>If I am not the benefitial owner of these shares I stipulate I have proof that I’ve been authorized to vote these shares by their benefitial owner(s).</p>
+                            <p>I stipulate I have not and will not accept anything of value in exchange for these votes, on penalty of confiscation of these tokens, and other penalties.</p>
+                            <p>I acknowledge that using any system of authomatic voting, re-voting, or vote refreshing, or allowing such system to be used on my behalf or on behalf of another, is forbidden and doing so violates this contract.</p>
+                          </div>
                         </div>
-                      </div>
-                      <div className="governance-vote__vote">
-                        <Button
-                          isUpper
-                          isStretched
-                          size="big"
-                          theme="red"
-                          text="Vote"
-                          onClick={() => this.setState({ votePopupVisibile: false })}
-                        />
-                      </div>
-                    </Panel>
+                      </Panel>
+                    </div>
+                    <div className="governance-vote__vote">
+                      <Button
+                        isUpper
+                        isStretched
+                        size="big"
+                        theme="red"
+                        text="Vote"
+                        onClick={() => this.setState({ votePopupVisibile: false })}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>

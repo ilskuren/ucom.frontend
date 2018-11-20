86f85b7b5dfb0b120c03f895f2d62ddbf291a12f
Pavel Ivanov <sudokey@gmail.com>
2018-11-20 12:13:56 +0300

    Fixed wallet amounts

--- src/components/Wallet/ProgressBar.jsx
+++ src/components/Wallet/ProgressBar.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const fixAmount = amount => Math.round(amount * 100) / 100;
+
 const ProgressBar = ({
   partAmount, fullAmount, title, description, label,
-}) => (
-  <div className="progress-bar">
-    <div className="toolbar">
-      <div className="toolbar__main">
-        <span className="progress-bar__amount ">
-          {`${partAmount} ${label} ${description || ''}`}
-        </span>
-      </div>
-      <div className="toolbar__side progress-bar__amount_small">
-        {`${fullAmount} ${label}`}
-      </div>
-    </div>
+}) => {
+  const fixedPartAmount = fixAmount(partAmount);
+  const fixedFullAmount = fixAmount(fullAmount);
 
-    <div className="progress-bar__container">
-      <div style={{ width: `${(partAmount * 100) / fullAmount}%` }} className="progress-bar__filler" />
+  return (
+    <div className="progress-bar">
+      <div className="toolbar">
+        <div className="toolbar__main">
+          <span className="progress-bar__amount ">
+            {`${fixedPartAmount} ${label} ${description || ''}`}
+          </span>
+        </div>
+        <div className="toolbar__side progress-bar__amount_small">
+          {`${fixedFullAmount} ${label}`}
+        </div>
+      </div>
+
+      <div className="progress-bar__container">
+        <div style={{ width: `${(fixedPartAmount * 100) / fixedFullAmount}%` }} className="progress-bar__filler" />
+      </div>
+      <div className="progress-bar__status">{title}</div>
     </div>
-    <div className="progress-bar__status">{title}</div>
-  </div>
-);
+  );
+};
 
 ProgressBar.propTypes = {
   title: PropTypes.string,

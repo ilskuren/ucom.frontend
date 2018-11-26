35c5daa911bb29f021d094b931094a057a579297
Pavel Ivanov <sudokey@gmail.com>
2018-11-26 15:50:03 +0300

    Fixed active badge label

--- src/utils/nodes.js
+++ src/utils/nodes.js
@@ -4,7 +4,7 @@ export const BP_STATUS_BACKUP_ID = 2;
 export const getBpStatusById = (id) => {
   switch (id) {
     case BP_STATUS_ACTIVE_ID:
-      return 'Actve';
+      return 'Active';
 
     case BP_STATUS_BACKUP_ID:
     default:

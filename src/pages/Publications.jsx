381cdb2b73387fa8126b1d31d20a0b9c3c125b13
dimalitvinov <dima8litvinov@gmail.com>
2018-12-07 18:35:48 +0300

    add UserList in publications feed

--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -1,10 +1,11 @@
 import { Route, Redirect } from 'react-router';
 import { NavLink } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import LayoutBase from '../components/Layout/LayoutBase';
 import Footer from '../components/Footer';
 import urls from '../utils/urls';
 import FeedCategories from '../components/Feed/FeedCategories';
+import UserList from '../components/User/UserList';
 import {
   POSTS_CATREGORIES_HOT_ID,
   POSTS_CATREGORIES_TRENDING_ID,
@@ -26,57 +27,73 @@ const POSTS_CATREGORIES = [{
   name: 'top',
 }];
 
-const Publications = props => (
-  <LayoutBase>
-    <div className="content-wrapper">
-      <div className="content content_base">
-        <div className="content__inner">
-          <div className="nav-bar">
-            <div className="nav-bar__title">
-              <h1 className="title">Publications</h1>
-            </div>
-            <div className="nav-bar__menu">
-              <div className="toolbar toolbar_responsive">
-                <div className="toolbar__main">
-                  <div className="menu menu_simple-tabs">
-                    {POSTS_CATREGORIES.map(item => (
-                      <div className="menu__item" key={item.id}>
-                        <NavLink
-                          className="menu__link"
-                          activeClassName="menu__link_active"
-                          to={urls.getPublicationsCategoryUrl(item.name)}
-                          isActive={() => props.location.pathname === urls.getPublicationsCategoryUrl(item.name)}
-                        >
-                          {item.name}
-                        </NavLink>
-                      </div>
-                    ))}
+const USERS_LIMIT = 5;
+
+const Publications = (props) => {
+  const [usersIds, setUserIds] = useState([]);
+  const sortUserIds = usersIds.filter((item, pos) => (
+    usersIds.indexOf(item) === pos
+  ));
+
+  return (
+    <LayoutBase>
+      <div className="content-wrapper">
+        <div className="content content_base">
+          <div className="content__inner">
+            <div className="nav-bar">
+              <div className="nav-bar__title">
+                <h1 className="title">Publications</h1>
+              </div>
+              <div className="nav-bar__menu">
+                <div className="toolbar toolbar_responsive">
+                  <div className="toolbar__main">
+                    <div className="menu menu_simple-tabs">
+                      {POSTS_CATREGORIES.map(item => (
+                        <div className="menu__item" key={item.id}>
+                          <NavLink
+                            className="menu__link"
+                            activeClassName="menu__link_active"
+                            to={urls.getPublicationsCategoryUrl(item.name)}
+                            isActive={() => props.location.pathname === urls.getPublicationsCategoryUrl(item.name)}
+                          >
+                            {item.name}
+                          </NavLink>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <hr className="content__separator" />
+          <hr className="content__separator" />
 
-        <div className="content__inner">
-          <div className="content__section content__section_small">
-            <Route exact path={urls.getPublicationsUrl()} render={() => <Redirect to={urls.getPublicationsCategoryUrl(POSTS_CATREGORIES[0].name)} />} />
-            {POSTS_CATREGORIES.map(item => (
-              <Route exact key={item.id} path={urls.getPublicationsCategoryUrl(item.name)} render={() => <FeedCategories categoryId={item.id} categoryName={item.name} />} />
-            ))}
+          <div className="grid grid_content">
+            <div className="grid__item grid__item_main">
+              <Route exact path={urls.getPublicationsUrl()} render={() => <Redirect to={urls.getPublicationsCategoryUrl(POSTS_CATREGORIES[0].name)} />} />
+              {POSTS_CATREGORIES.map(item => (
+                <Route exact key={item.id} path={urls.getPublicationsCategoryUrl(item.name)} render={() => <FeedCategories onUsersAdd={data => setUserIds(usersIds.concat(data))} categoryId={item.id} categoryName={item.name} />} />
+              ))}
+            </div>
+            <div className="grid__item grid__item_side">
+              <div className="feed_side">
+                <div className="sidebar">
+                  <UserList usersIds={sortUserIds} limit={USERS_LIMIT} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="content">
-      <div className="content__inner">
-        <Footer />
+      <div className="content">
+        <div className="content__inner">
+          <Footer />
+        </div>
       </div>
-    </div>
-  </LayoutBase>
-);
+    </LayoutBase>
+  );
+};
 
 export default Publications;

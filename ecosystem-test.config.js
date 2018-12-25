647468aeb8d3c4b2b51907491a61632b5a737d91
Vladimir Ice <mr.vladimir.ice@gmail.com>
2018-12-25 17:44:48 +0300

    pm2 ecosystem files are added - for test (local) and for staging

---
+++ ecosystem-test.config.js
@@ -0,0 +1,20 @@
+const NODE_ENV = 'staging';
+const HTTP_SERVER_PORT = 3020;
+const IGNORE_WATCH = ['node_modules', 'public', 'logs'];
+
+module.exports = {
+  apps: [
+    {
+      name: `${NODE_ENV}_frontend_renderer`,
+      instance_var: 'INSTANCE_ID',
+      script: 'server.js',
+      watch: true,
+      ignore_watch: IGNORE_WATCH,
+      env: {
+        PORT: HTTP_SERVER_PORT,
+        NODE_ENV,
+        autorestart: true,
+      },
+    },
+  ],
+};

acbdbfb89a220df24c68c78ae7d34257f6e38656
andreyelek <sukanov2015@yandex.ru>
2018-12-27 17:01:22 +0300

    pagination

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import Pagination from 'rc-pagination';
+// import 'rc-pagination/assets/index.css';
 import UserCard from '../components/UserCard';
 import LayoutBase from '../components/Layout/LayoutBase';
 import api from '../api';
@@ -10,7 +12,17 @@ import IconTableTriangle from '../components/Icons/TableTriangle';
 import { getFileUrl } from '../utils/upload';
 import loader from '../utils/loader';
 
-const UsersPage = () => {
+const textItemRender = (current, type, element) => {
+  if (type === 'prev') {
+    return 'Prev';
+  }
+  if (type === 'next') {
+    return 'Next';
+  }
+  return element;
+};
+
+const UsersPage = (props) => {
   const [usersData, setUsersData] = useState({ data: [], metadata: {} });
 
   const history = createBrowserHistory();
@@ -18,6 +30,13 @@ const UsersPage = () => {
   const page = urlParams.get('page') || 1;
   const sortBy = urlParams.get('sortBy') || '-current_rate';
   const perPage = urlParams.get('perPage') || 20;
+
+  const getLink = params => (`/users?page=${params.page || page}&sortBy=${params.sortBy || sortBy}&perPage=${params.perPage || perPage}`);
+
+  const onChangePage = (current) => {
+    props.history.push(getLink({ page: current }));
+  };
+
   const setPaging = async (params) => {
     loader.start();
 
@@ -32,7 +51,6 @@ const UsersPage = () => {
   };
 
   useEffect(() => {
-    console.log(history);
     setPaging({ page, perPage, sortBy });
   }, [history.location.search]);
 
@@ -40,7 +58,7 @@ const UsersPage = () => {
   const {
     data: users,
   } = usersData;
-  const { hasMore } = usersData.metadata;
+  const { hasMore, totalAmount } = usersData.metadata;
   return (
     <LayoutBase>
       <div className="content">
@@ -48,9 +66,8 @@ const UsersPage = () => {
           <div className="content__title content__title_narrow">
             <h1 className="title">People</h1>
           </div>
-          {// users && users.length > 0 &&
-          1 &&
-            <div className="table-content">
+          {users && users.length > 0 &&
+            <div className="table-content_big-bottom">
               <div className="table-content__table">
                 <table className="list-table list-table_indexed list-table_users list-table_responsive">
                   <thead className="list-table__head">
@@ -72,23 +89,24 @@ const UsersPage = () => {
                             'list-table__cell',
                             { 'list-table__cell_sortable': item.sortable },
                           )}
-                          onClick={() => this.setPaging({ sortBy: `${sortBy === `-${item.name}` ? '' : '-'}${item.name}` })}
                         >
-                          <div className="list-table__title">
-                            {item.title}
+                          <Link to={getLink({ sortBy: `${sortBy === `-${item.name}` ? '' : '-'}${item.name}` })}>
+                            <div className="list-table__title">
+                              {item.title}
 
-                            {sortBy === `-${item.name}` && (
-                              <div className="list-table__sort-icon">
-                                <IconTableTriangle />
-                              </div>
-                            )}
+                              {sortBy === `-${item.name}` && (
+                                <div className="list-table__sort-icon">
+                                  <IconTableTriangle />
+                                </div>
+                              )}
 
-                            {sortBy === `${item.name}` && (
-                              <div className="list-table__sort-icon list-table__sort-icon_flip">
-                                <IconTableTriangle />
-                              </div>
-                            )}
-                          </div>
+                              {sortBy === `${item.name}` && (
+                                <div className="list-table__sort-icon list-table__sort-icon_flip">
+                                  <IconTableTriangle />
+                                </div>
+                              )}
+                            </div>
+                          </Link>
                         </td>
                       ))}
                     </tr>
@@ -114,28 +132,17 @@ const UsersPage = () => {
                   </tbody>
                 </table>
               </div>
-              {(+page - 1) ?
-                <div className="table-content__showmore">
-                  <Link className="auth__link" to={`/users?page=${+page - 1}`}>Prev</Link>
-                </div> : null
-              }
+              <div className="table-content__navbar">
+                {hasMore && (
+                  <div className="table-content__showmore">
+                    <div className="button-clean button-clean_link">
+                      <Link to={getLink({ perPage: +perPage + 20 })}>Show More</Link>
+                    </div>
+                  </div>
+                )}
+                <Pagination className="table-content__pagination" showTitle={false} total={totalAmount} pageSize={+perPage} itemRender={textItemRender} current={+page} onChange={onChangePage} />
 
-              {hasMore &&
-                <div className="table-content__showmore">
-                  <Link className="auth__link" to={`/users?page=${+page + 1}`}>Next</Link>
-                </div>
-              }
-
-              {hasMore && (
-                <div className="table-content__showmore">
-                  <button
-                    className="button-clean button-clean_link"
-                    onClick={() => this.setPaging({ perPage: perPage + 20 })}
-                  >
-                    Show More
-                  </button>
-                </div>
-              )}
+              </div>
             </div>
           }
         </div>

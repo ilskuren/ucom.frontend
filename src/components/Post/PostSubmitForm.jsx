600a374c8117a2bccbea189b43a6b7dc8e424106
Pavel Ivanov <sudokey@gmail.com>
2019-01-11 11:49:14 +0300

    Added post submit form cover

--- src/components/Post/PostSubmitForm.jsx
+++ src/components/Post/PostSubmitForm.jsx
@@ -4,18 +4,24 @@ import TextareaAutosize from '../TextareaAutosize';
 export default () => (
   <div className="post-submit-form">
     <div className="post-submit-form__title">
-      <h3 className="title title_light title_small">Post preview</h3>
+      <h3 className="title title_small">Post preview</h3>
     </div>
 
-    {/* <div className="post-submit-form__cover">
-      <input type="input" />
-    </div> */}
+    <div className="post-submit-form__cover">
+      <div className="post-submit-cover">
+        <label className="post-submit-cover__inner">
+          <div className="post-submit-cover__text">Click for upload preview cover</div>
+          {/* <div className="post-submit-cover__change">Change cover</div>
+          <input type="file" /> */}
+        </label>
+      </div>
+    </div>
 
     <div className="post-submit-form__field">
       <TextareaAutosize
         rows="1"
         placeholder="Preview title"
-        className="post-submit-form__data"
+        className="post-submit-form__data post-submit-form__data_title"
       />
     </div>
 
@@ -23,7 +29,7 @@ export default () => (
       <TextareaAutosize
         rows="1"
         placeholder="Preview sub title"
-        className="post-submit-form__data"
+        className="post-submit-form__data post-submit-form__data_lead"
       />
     </div>
   </div>

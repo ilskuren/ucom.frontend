ae65f39a5d04894ce6ca53c82bb51db9e6342121
andreyelek <sukanov2015@yandex.ru>
2019-01-16 11:35:10 +0300

    fixed debounce

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import IconSearch from './Icons/Search';
 import IconClose from './Icons/Close';
 
-const SearchInput = ({ search, setSearch }) => (
-  <div className="input-search">
-    <input
-      className="input-search__text"
-      type="text"
-      value={search}
-      onChange={e => setSearch(e.target.value)}
-      placeholder="Search"
-    />
+const debounce = (fn, time) => {
+  let timeout;
 
-    {search.length ?
-      <div role="presentation" className="input-search__icon input-search__icon_close" onClick={() => setSearch('')}><IconClose /></div> :
-      <div className="input-search__icon"><IconSearch /></div>
+  return function temp(...args) {
+    const functionCall = () => fn.apply(this, args);
+
+    clearTimeout(timeout);
+    timeout = setTimeout(functionCall, time);
+  };
+};
+
+class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.searchInput = createRef();
+    this.debouncedSetSearch = debounce(this.props.setSearch, 500);
+  }
+
+  componentDidMount() {
+    const { search } = this.props;
+    if (this.searchInput.current) {
+      if (search !== this.searchInput.current.value) {
+        this.searchInput.current.value = search;
+      }
     }
-  </div>
-);
+  }
+
+  render() {
+    const { search } = this.props;
+    return (
+      <div className="input-search">
+        <input
+          className="input-search__text"
+          type="text"
+          ref={this.searchInput}
+          onChange={e => this.debouncedSetSearch(e.target.value)}
+          placeholder="Search"
+        />
+
+        {search.length || (this.searchInput.current && this.searchInput.current.value.length) ?
+          <div
+            role="presentation"
+            className="input-search__icon input-search__icon_close"
+            onClick={() => {
+              this.searchInput.current.value = '';
+              this.props.setSearch('');
+            }}
+          >
+            <IconClose />
+          </div> :
+          <div className="input-search__icon"><IconSearch /></div>
+        }
+      </div>
+    );
+  }
+}
+
 export default SearchInput;
